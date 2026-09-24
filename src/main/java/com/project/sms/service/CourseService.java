317f503332693e3b3652741b09b54c13c4481807
Thuy Mai <thuymmai11@gmail.com>
2026-09-24 11:59:35 -0500

    Changes

--- src/main/java/com/project/sms/service/CourseService.java
+++ src/main/java/com/project/sms/service/CourseService.java
@@ -1,4 +1,8 @@
 package com.project.sms.service;
 
+import com.project.sms.dto.CourseDTO;
+
 public interface CourseService {
+
+    CourseDTO createCourse(CourseDTO courseDTO);
 }
--- src/main/java/com/project/sms/service/impl/CourseServiceImpl.java
+++ src/main/java/com/project/sms/service/impl/CourseServiceImpl.java
@@ -1,6 +1,25 @@
 package com.project.sms.service.impl;
 
+import com.project.sms.dto.CourseDTO;
+import com.project.sms.repository.CourseRepository;
 import com.project.sms.service.CourseService;
+import org.modelmapper.ModelMapper;
 
 public class CourseServiceImpl implements CourseService {
+
+    private final CourseRepository courseRepository;
+    private final ModelMapper mapper;
+
+    // created constructor
+    CourseServiceImpl(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+
+    }
+
+    @Override
+    public CourseDTO createCourse(CourseDTO courseDTO) {
+        courseRepository.save(null);
+
+        return null;
+    }
 }
